db1cdeb57e7e1410b7865745920f7e58b51a2617
Sajjad Toomari <toomaris@gmail.com>
2022-07-20 11:47:39 +0430

    run only at night

--- HLS/Worker.cs
+++ HLS/Worker.cs
@@ -14,176 +14,197 @@ public class Worker : BackgroundService
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            try
+            if (DateTime.Now.Hour >= 22 && DateTime.Now.Hour < 9)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-
-                #region Get Folders
-
-                _logger.LogInformation("getting folders", DateTimeOffset.Now);
-
-                var ffmpegLocation = _configuration.GetSection("Folders:FFMPEGFolder").Value;
-                var videoPath = _configuration.GetSection("Folders:VideoFolder").Value;
-                var hlsPath = _configuration.GetSection("Folders:HlsFolder").Value;
-                var thread = int.Parse(_configuration.GetSection("Thread").Value);
-
-                #endregion
-
-                #region Create Hls Path
-
-                _logger.LogInformation("Creating folders if not exists", DateTimeOffset.Now);
-
-                if (!Directory.Exists(hlsPath))
+                try
                 {
-                    Directory.CreateDirectory(hlsPath);
-                }
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
-                #endregion
+                    #region Get Folders
 
-                #region Get Video
+                    _logger.LogInformation("getting folders", DateTimeOffset.Now);
 
-                _logger.LogInformation("Getting videos", DateTimeOffset.Now);
+                    var ffmpegLocation = _configuration.GetSection("Folders:FFMPEGFolder").Value;
+                    var videoPath = _configuration.GetSection("Folders:VideoFolder").Value;
+                    var hlsPath = _configuration.GetSection("Folders:HlsFolder").Value;
+                    var thread = int.Parse(_configuration.GetSection("Thread").Value);
 
-                var videos = Directory.GetFiles(videoPath, "*.mp4", SearchOption.TopDirectoryOnly);
+                    #endregion
 
-                _logger.LogInformation($"All MP4 Files : {videos.Length}", DateTimeOffset.Now);
+                    #region Create Hls Path
 
-                var hlsVideos = Directory.GetFiles(hlsPath, "*.m3u8", SearchOption.TopDirectoryOnly);
+                    _logger.LogInformation("Creating folders if not exists", DateTimeOffset.Now);
 
-                _logger.LogInformation($"All HLS Files : {hlsVideos.Length}", DateTimeOffset.Now);
-
-                #endregion
-
-                #region Start Proccessing
-
-                var tasks = new List<Task>();
-
-                var index = 0;
-
-                await Parallel.ForEachAsync(videos, new ParallelOptions { MaxDegreeOfParallelism = thread }, async (video, ct) =>
-                {
-                    var thisindex = 0;
-
-                    lock (_lockObject)
+                    if (!Directory.Exists(hlsPath))
                     {
-                        index++;
-                        thisindex = index;
-
-                        _logger.LogInformation($"Starting Video Number {index}", DateTimeOffset.Now);
-
-                        var videoName = video.Split("\\")[^1].Split(".")[0];
-
-                        //the video is proccesed skipping...
-                        if (File.Exists($"{hlsPath}\\{videoName}\\video.m3u8"))
-                        {
-                            _logger.LogInformation($"Skipping Video Number {index} because it's converted before", DateTimeOffset.Now);
-                            return;
-                        }
+                        Directory.CreateDirectory(hlsPath);
                     }
 
-                    try
+                    #endregion
+
+                    #region Get Video
+
+                    _logger.LogInformation("Getting videos", DateTimeOffset.Now);
+
+                    var videos = Directory.GetFiles(videoPath, "*.mp4", SearchOption.TopDirectoryOnly);
+
+                    _logger.LogInformation($"All MP4 Files : {videos.Length}", DateTimeOffset.Now);
+
+                    var hlsVideos = Directory.GetFiles(hlsPath, "*.m3u8", SearchOption.TopDirectoryOnly);
+
+                    _logger.LogInformation($"All HLS Files : {hlsVideos.Length}", DateTimeOffset.Now);
+
+                    #endregion
+
+                    #region Start Proccessing
+
+                    var tasks = new List<Task>();
+
+                    var index = 0;
+
+                    await Parallel.ForEachAsync(videos, new ParallelOptions { MaxDegreeOfParallelism = thread }, async (video, _) =>
                     {
-                        #region Sample Command
-
-                        //ffmpeg -i some_fun_video_name.mp4 -profile:v baseline -level 3.0 -s 800x480 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls ./media/some_fun_video_name/hls/480_out.m3u8
-                        //ffmpeg -i some_fun_video_name.mp4 -profile:v baseline -level 3.0 -s 960x540 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls ./media/some_fun_video_name/hls/540_out.m3u8
-                        //ffmpeg -i some_fun_video_name.mp4 -profile:v baseline -level 3.0 -s 1280x720 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls ./media/some_fun_video_name/hls/720_out.m3u8
-
-                        #endregion
-
-                        var videoName = video.Split("\\")[^1].Split(".")[0];
-
-                        //ffmpeg commands for converting to hls
-                        var commands = new string[]
+                        if (!(DateTime.Now.Hour >= 22 && DateTime.Now.Hour < 9))
                         {
-                        $"-i {video} -profile:v baseline -level 3.0 -s 800x480 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls {hlsPath}\\{videoName}\\480_out.m3u8",
-                        $"-i {video} -profile:v baseline -level 3.0 -s 960x540 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls {hlsPath}\\{videoName}\\540_out.m3u8",
-                        $"-i {video} -profile:v baseline -level 3.0 -s 1280x720 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls {hlsPath}\\{videoName}\\720_out.m3u8"
-                        };
+                            return;
+                        }
 
-                        Directory.CreateDirectory($"{hlsPath}\\{videoName}");
+                        var thisindex = 0;
 
-                        var isAllDoneSuccessfully = true;
-
-                        //starting proccess
-                        foreach (var command in commands)
+                        lock (_lockObject)
                         {
-                            var startInfo = new ProcessStartInfo($"{ffmpegLocation}\\ffmpeg.exe", "-y " + command);
+                            index++;
+                            thisindex = index;
 
-                            startInfo.CreateNoWindow = true;
-                            startInfo.RedirectStandardInput = true;
-                            startInfo.RedirectStandardOutput = true;
-                            startInfo.RedirectStandardError = true;
-                            startInfo.UseShellExecute = false;
-                            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            _logger.LogInformation($"Starting Video Number {index}", DateTimeOffset.Now);
 
-                            var process = new Process() { StartInfo = startInfo, EnableRaisingEvents = true };
+                            var videoName = video.Split("\\")[^1].Split(".")[0];
 
-                            var exitCode = await process.WaitForExitAsync(true, null);
-
-                            if (exitCode != 0)
+                            //the video is proccesed skipping...
+                            if (File.Exists($"{hlsPath}\\{videoName}\\video.m3u8"))
                             {
-                                isAllDoneSuccessfully = false;
-                                break;
+                                _logger.LogInformation($"Skipping Video Number {index} because it's converted before", DateTimeOffset.Now);
+                                return;
                             }
                         }
 
-                        //creating final m3u8 file
-                        if (isAllDoneSuccessfully)
+                        try
                         {
-                            using var streamWriter = new StreamWriter($"{hlsPath}\\{videoName}\\video.m3u8", false, Encoding.UTF8);
-                            await streamWriter.WriteLineAsync("#EXTM3U");
-                            await streamWriter.WriteLineAsync("#EXT-X-STREAM-INF:BANDWIDTH=750000,RESOLUTION=854x480");
-                            await streamWriter.WriteLineAsync("480_out.m3u8");
-                            await streamWriter.WriteLineAsync("#EXT-X-STREAM-INF:BANDWIDTH=1200000,RESOLUTION=960x540");
-                            await streamWriter.WriteLineAsync("540_out.m3u8");
-                            await streamWriter.WriteLineAsync("#EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=1280x720");
-                            await streamWriter.WriteLineAsync("720_out.m3u8");
-                            await streamWriter.FlushAsync();
-                            streamWriter.Close();
+                            #region Sample Command
+
+                            //ffmpeg -i some_fun_video_name.mp4 -profile:v baseline -level 3.0 -s 800x480 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls ./media/some_fun_video_name/hls/480_out.m3u8
+                            //ffmpeg -i some_fun_video_name.mp4 -profile:v baseline -level 3.0 -s 960x540 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls ./media/some_fun_video_name/hls/540_out.m3u8
+                            //ffmpeg -i some_fun_video_name.mp4 -profile:v baseline -level 3.0 -s 1280x720 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls ./media/some_fun_video_name/hls/720_out.m3u8
+
+                            #endregion
+
+                            var videoName = video.Split("\\")[^1].Split(".")[0];
+
+                            //ffmpeg commands for converting to hls
+                            var commands = new string[]
+                            {
+                        $"-i {video} -profile:v baseline -level 3.0 -s 800x480 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls {hlsPath}\\{videoName}\\480_out.m3u8",
+                        $"-i {video} -profile:v baseline -level 3.0 -s 960x540 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls {hlsPath}\\{videoName}\\540_out.m3u8",
+                        $"-i {video} -profile:v baseline -level 3.0 -s 1280x720 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls {hlsPath}\\{videoName}\\720_out.m3u8"
+                            };
+
+                            Directory.CreateDirectory($"{hlsPath}\\{videoName}");
+
+                            var isAllDoneSuccessfully = true;
+
+                            //starting proccess
+                            foreach (var command in commands)
+                            {
+                                if (!(DateTime.Now.Hour >= 22 && DateTime.Now.Hour < 9))
+                                {
+                                    isAllDoneSuccessfully = false;
+                                    break;
+                                }
+                                else
+                                {
+                                    var startInfo = new ProcessStartInfo($"{ffmpegLocation}\\ffmpeg.exe", "-y " + command);
+
+                                    startInfo.CreateNoWindow = true;
+                                    startInfo.RedirectStandardInput = true;
+                                    startInfo.RedirectStandardOutput = true;
+                                    startInfo.RedirectStandardError = true;
+                                    startInfo.UseShellExecute = false;
+                                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                                    var process = new Process() { StartInfo = startInfo, EnableRaisingEvents = true };
+
+                                    var exitCode = await process.WaitForExitAsync(true, null);
+
+                                    if (exitCode != 0)
+                                    {
+                                        isAllDoneSuccessfully = false;
+                                        break;
+                                    }
+                                }                                
+                            }
+
+                            //creating final m3u8 file
+                            if (isAllDoneSuccessfully)
+                            {
+                                using var streamWriter = new StreamWriter($"{hlsPath}\\{videoName}\\video.m3u8", false, Encoding.UTF8);
+                                await streamWriter.WriteLineAsync("#EXTM3U");
+                                await streamWriter.WriteLineAsync("#EXT-X-STREAM-INF:BANDWIDTH=750000,RESOLUTION=854x480");
+                                await streamWriter.WriteLineAsync("480_out.m3u8");
+                                await streamWriter.WriteLineAsync("#EXT-X-STREAM-INF:BANDWIDTH=1200000,RESOLUTION=960x540");
+                                await streamWriter.WriteLineAsync("540_out.m3u8");
+                                await streamWriter.WriteLineAsync("#EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=1280x720");
+                                await streamWriter.WriteLineAsync("720_out.m3u8");
+                                await streamWriter.FlushAsync();
+                                streamWriter.Close();
+                            }
+                            else
+                            {
+                                //delete because proccess done with error
+                                if (Directory.Exists($"{hlsPath}\\{videoName}"))
+                                {
+                                    Directory.Delete($"{hlsPath}\\{videoName}", true);
+                                }
+                            }
+
+                            if (isAllDoneSuccessfully)
+                            {
+                                _logger.LogInformation($"End video number {thisindex}", DateTimeOffset.Now);
+
+                            }
+                            else
+                            {
+                                if (!(DateTime.Now.Hour >= 22 && DateTime.Now.Hour < 9))
+                                {
+                                    _logger.LogInformation($"Skip video number {thisindex} because it's out of running time", DateTimeOffset.Now);
+                                }
+                                else
+                                {
+                                    _logger.LogInformation($"End video number {thisindex} with error", DateTimeOffset.Now);
+                                }
+                            }
+
                         }
-                        else
+                        catch (Exception ex)
                         {
-                            //delete because proccess done with error
+                            _logger.LogError(ex.Message + ";;;" + ex.InnerException?.Message + ";;;" + ex.InnerException?.InnerException?.Message);
+
+                            var videoName = video.Split("\\")[^1].Split(".")[0];
+
                             if (Directory.Exists($"{hlsPath}\\{videoName}"))
                             {
                                 Directory.Delete($"{hlsPath}\\{videoName}", true);
                             }
                         }
 
-                        if (isAllDoneSuccessfully)
-                        {
-                            _logger.LogInformation($"End video number {thisindex}", DateTimeOffset.Now);
-
-                        }
-                        else
-                        {
-                            _logger.LogInformation($"End video number {thisindex} with error", DateTimeOffset.Now);
-                        }
-
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex.Message + ";;;" + ex.InnerException?.Message + ";;;" + ex.InnerException?.InnerException?.Message);
-
-                        var videoName = video.Split("\\")[^1].Split(".")[0];
-
-                        if (Directory.Exists($"{hlsPath}\\{videoName}"))
-                        {
-                            Directory.Delete($"{hlsPath}\\{videoName}", true);
-                        }
-                    }
-
-                });
-
-                await Task.Delay(5000);
+                    });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message + ";;;" + ex.InnerException?.Message + ";;;" + ex.InnerException?.InnerException?.Message);
+                    await Task.Delay(5000);
+                }
             }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message + ";;;" + ex.InnerException?.Message + ";;;" + ex.InnerException?.InnerException?.Message);
-                await Task.Delay(5000);
-            }
-
+            await Task.Delay(5000);
             #endregion
         }
     }
